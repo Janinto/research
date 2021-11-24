9f3d6d0a88fb78c6da267bfd476197a38e5c69e5
sino1741 <sino1741@gmail.com>
2021-11-24 12:18:35 +0900

    addedAnim

--- android/ViewPropertyAnimator/app/src/main/java/com/example/viewpropertyanimator/MainActivity.java
+++ android/ViewPropertyAnimator/app/src/main/java/com/example/viewpropertyanimator/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 
-// 트윈 애니메이션 : 커졌다 작아졌다하는 애니메이션
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+// 트윈 애니메이션 : 커졌다 작아졌다하는 애니메이션
         //버튼 참조
         Button scaleButton = findViewById(R.id.scaleButton);
         //버튼 리스너
@@ -32,5 +32,35 @@ public class MainActivity extends AppCompatActivity {
                 view.startAnimation(anim);
             }
         });
+// rotate 돌아가는 애니메이션
+        Button las = findViewById(R.id.las);
+
+        las.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public  void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.rotate);
+                view.startAnimation(anim);
+            }
+
+        });
+// 점점 사라지는 애니메이션
+        Button laa = findViewById(R.id.laa);
+
+        laa.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.alpha);
+                view.startAnimation(anim);
+            }
+        });
+
+
+
     }
 }
