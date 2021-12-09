f8c03d2916b0a45ac8701953cc9ed2fb89374d14
sino1741 <sino1741@gmail.com>
2021-12-09 18:10:28 +0900

    realtab

---
+++ Astron/TabApp/app/src/main/java/com/example/tabapp/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.tabapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment1, fragment2, fragment3, fragment4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragment4 = new Fragment4();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, fragment1).commit();
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabBar);
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+                 if (position == 0){
+
+                    selected = fragment1;
+
+                }else if (position == 1){
+
+                    selected = fragment2;
+
+                }else if (position == 2){
+
+                    selected = fragment3;
+                }else if (position == 3){
+
+                     selected = fragment4;
+                 }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
