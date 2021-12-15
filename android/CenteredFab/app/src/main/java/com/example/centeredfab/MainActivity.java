7ff00709bf3005c57f6d6fdce229b0afa7eeed66
sino1741 <sino1741@gmail.com>
2021-12-15 12:04:26 +0900

    VolumeControl

--- android/CenteredFab/app/src/main/java/com/example/centeredfab/MainActivity.java
+++ android/CenteredFab/app/src/main/java/com/example/centeredfab/MainActivity.java
@@ -11,18 +11,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    //TextView textView, textView1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        //textView = findViewById(R.id.textView);
 
         SeekBar seekBar = findViewById(R.id.seekBar);
 
-        final  AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
         int nMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int nCurrentVolume = audioManager
                 .getStreamVolume(AudioManager.STREAM_MUSIC);
@@ -36,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
-                        progress, 0);
+                        progress, AudioManager.FLAG_SHOW_UI);
 
             }
 
@@ -48,27 +49,66 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
-                textView.setText(String.format("선택 값은 %d 입니다.", seekBar.getProgress()));
+                //textView.setText(String.format("미디어 선택 값은 %d 입니다.", seekBar.getProgress()));
+            }
+        });
+
+        //textView1 = findViewById(R.id.textView1);
+
+        SeekBar seekBar1 = findViewById(R.id.seekBar1);
+
+        AudioManager audioManager1 = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int nMax1 = audioManager1.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION);
+        int nCurrentVolume1 = audioManager1
+                .getStreamVolume(AudioManager.STREAM_NOTIFICATION);
+
+        seekBar1.setMax(nMax1);
+        seekBar1.setProgress(nCurrentVolume1);
+
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar1, int progress1, boolean fromUser1) {
+
+                audioManager1.setStreamVolume(AudioManager.STREAM_NOTIFICATION,
+                        progress1, AudioManager.FLAG_SHOW_UI);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar1) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar1) {
+
+                //textView1.setText(String.format("알림 선택 값은 %d 입니다.", seekBar1.getProgress()));
             }
         });
 
 
 
+
+
+
+
     }
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        AudioManager mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_VOLUME_UP :
-                mAudioManager.adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
-                return true;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-                mAudioManager.adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
-                return true;
-            case KeyEvent.KEYCODE_BACK:
-                return true;
-        }
-        return false;
-    }
+    // 액티비티에 원하는 볼륨 종류를 정해서 키로 조절가능한 기능
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        AudioManager mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+//        switch (keyCode) {
+//            case KeyEvent.KEYCODE_VOLUME_UP :
+//                mAudioManager.adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+//                return true;
+//            case KeyEvent.KEYCODE_VOLUME_DOWN:
+//                mAudioManager.adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+//                return true;
+//            case KeyEvent.KEYCODE_BACK:
+//                return true;
+//        }
+//        return false;
+//    }
 
 
 
